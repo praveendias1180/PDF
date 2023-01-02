dcb2aa9808a3c18084dfa24b6261859110acfa8d
Praveen Dias <praveendias1180@gmail.com>
2023-01-02 06:45:03 +0530

    The printer

--- src/PDF.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Test PDF Class
- */
-class PDF{
-    function Writer(){
-        echo 'Hello from Writer';
-    }
-}
---
+++ src/Printer.php
@@ -0,0 +1,9 @@
+<?php namespace Praveendias1180\Pdf;
+/**
+ * Test Printer Class
+ */
+class Printer{
+    function print(){
+        echo 'Hello from Printer';
+    }
+}
